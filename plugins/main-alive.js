739fe76b5374d9783900b0de36254b1c45367ed5
ΛＳＬΛ Ｍ <unknownmansion321@gmail.com>
2023-09-29 18:49:31 +0530

    Update main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -4,10 +4,10 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
 
     // Sound
     let name = m.pushName || conn.getName(m.sender)
-    var vn = "https://raw.githubusercontent.com/Guru322/GURU-BOT/main/src/mp3/Guru1.mp3"
-    let url = "https://github.com/Guru322/GURU-BOT"
-    let murl = "https://youtu.be/DibiLc17dh0?si=xp9bQ-_frEyDB1-i"
-    let img = "https://avatars.githubusercontent.com/u/106463398?v=4"
+    var vn = "https://i.imgur.com/UuQfInm.mp3"
+    let url = "https://github.com/false-eye"
+    let murl = "https://Instagram.com/false_eye"
+    let img = "https://telegra.ph/file/c108f6e9d7a5296127736.jpg"
     let con = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
     let doc = {
         audio: {
@@ -23,7 +23,7 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
             showAdAttribution: true,
             mediaType: 2,
             mediaUrl: murl,
-            title: "I AM ALIVE",
+            title: "Not Dead! :)",
             body: botname,
             ptt: true,
             sourceUrl: url,
