0c9c44aa166ab6971e174e092bc8a992773d1a71
ΛＳＬΛ Ｍ <unknownmansion321@gmail.com>
2023-09-29 19:23:50 +0530

    Update main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -6,7 +6,7 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
     let name = m.pushName || conn.getName(m.sender)
     var vn = "https://i.imgur.com/UuQfInm.mp3"
     let url = "https://github.com/false-eye"
-    let murl = "https://Instagram.com/false_eye"
+    let murl = "https://telegra.ph/file/683d9f5ec34ec7307950c.mp4"
     let img = "https://telegra.ph/file/c108f6e9d7a5296127736.jpg"
     let con = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
     let doc = {
