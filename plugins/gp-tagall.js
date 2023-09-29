22339dbcb63ec277fc1c5d76e6a9aca85dd1d5b3
ΛＳＬΛ Ｍ <unknownmansion321@gmail.com>
2023-09-29 19:31:31 +0530

    Update gp-tagall.js

--- plugins/gp-tagall.js
+++ plugins/gp-tagall.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, participants, isAdmin, isOwner, groupMetadata }) => {
     let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
-    m.reply(`▢ Group : *${groupMetadata.subject}*\n▢ Members : *${participants.length}*${text ? `\n▢ Message : ${text}\n` : ''}\n┌───⊷ *MENTIONS*\n` + users.map(v => '▢ @' + v.replace(/@.+/, '')).join`\n` + '\n└──✪ 𝟗𝑖𝑛𝑒 𝑥𝟑 x 𝑓𝑎𝑙𝑠𝑒 𝑒𝑦𝑒 ✪──', null, {
+    m.reply(`𒈞 Group : *${groupMetadata.subject}*\n𒈞 Members : *${participants.length}*${text ? `\n𒈞 Message : ${text}\n` : ''}\n┌───⊷ *MENTIONS*\n` + users.map(v => '▢ @' + v.replace(/@.+/, '')).join`\n` + '\n└──✪ 𝟗𝑖𝑛𝑒 𝑥𝟑 x 𝑓𝑎𝑙𝑠𝑒 𝑒𝑦𝑒 ✪──', null, {
         mentions: users
     })
 }
