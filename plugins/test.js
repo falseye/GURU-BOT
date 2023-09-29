80493e1e1f5ea7da4163ddd28b32478c8748abfd
ΛＳＬΛ Ｍ <unknownmansion321@gmail.com>
2023-09-29 19:20:09 +0530

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import fetch from 'node-fetch'
 let handler  = async (m, { conn, usedPrefix: _p }) => {
-    let img = "https://avatars.githubusercontent.com/u/106463398?v=4"
+    let img = "https://telegra.ph/file/7660b101cf5f9283a6b13.jpg"
 let info = `*BOT ACTIVE*`
 await conn.reply(m.chat, info, m, { contextInfo: { mentionedJid: [m.sender],forwardingScore: 256,
       isForwarded: true, externalAdReply: { title: author, body: botname, sourceUrl: fgyt, thumbnail: await conn.getFile(img) }}})
